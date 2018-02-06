6d0c2894106334f181d2b8d75e1f3cc85539549e
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-06 16:31:29 +0800

    change policy

--- lib/Notification.sh
+++ lib/Notification.sh
@@ -3,8 +3,8 @@ Notification()
 {
     read -p "$1" ans
     case $ans in
-        y*|Y*) printf "$2";;
-        *) exit 0;;
+        y*|Y*) printf "$2" && return 0;;
+        *) return 1;;
     esac
 }
 
