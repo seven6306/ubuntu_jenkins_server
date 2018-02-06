ebb02e0c17e44272585d99f256e77efc7e95e7c4
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-06 16:59:55 +0800

    merge SSL function

--- jenkins_server.sh
+++ jenkins_server.sh
@@ -7,6 +7,8 @@
 . lib/NetworkConnTest.sh
 . lib/declare_variables.sh
 COUNTER=10
+PORT=8080
+PROTOCOL=http
 
 CheckInstall Jenkins --install "/etc/init.d/jenkins" "/var/lib/jenkins,/usr/share/jenkins"
 CheckPermission && NetworkConnTest www.google.com
@@ -37,8 +39,25 @@ do  for i in '.' '..' '...' '....'
 done
 JavaVer=`java -version 2>&1 | grep "java version" | awk -F\" '{print $2}'`
 [ `service jenkins status | grep -co not` -ne 0 ] && printf "\n${RED}Sorry, jenkins server is unavailable...${NC}\n\n" && exit 1
+Notification "Configure jenkins server with SSL? (default:No) [y/N] : " "${PURPLE}Configuring SSL settings...${NC}\n${LINE}\n\n"
+if [ $? -eq 0 ]; then
+    if [ `dpkg -l | grep -c nginx` -gt 1 -a -f /etc/nginx/sites-enabled/default ]; then
+        sed -i 's,try_files $uri $uri/ =404;,# try_files $uri $uri/ =404;,g' /etc/nginx/sites-enabled/default
+        for each_line in "proxy_redirect      http://localhost:8080 https://`GethostIPAddr`;" 'proxy_read_timeout  90;' 'proxy_pass          http://localhost:8080;' '# Fix the “It appears that your reverse proxy set up is broken" error.'
+        do  sed -i "/\#\ include\ \/etc\/nginx\/naxsi\.rules/ a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ ${each_line}" /etc/nginx/sites-enabled/default
+        done
+        sed -i 's,\--webroot=\/var\/cache\/$NAME\/war\ \--httpPort=\$HTTP_PORT,\--webroot=\/var\/cache\/$NAME\/war\ \--httpPort=\$HTTP_PORT\ \--httpListenAddress=127\.0\.0\.1\ \-ajp13Port=\$AJP_PORT,g' /etc/default/jenkins
+        service nginx restart
+        service jenkins restart
+        case $? in
+            0) printf "\n${LINE}\n%s\t%31s\033[0;32m %s \033[0m]\n\n" " * Configure jenkins server with SSL" "[" "OK" && PORT=443 && PROTOCOL=https;;
+            *) printf "\n${LINE}\n%s\t%31s\033[0;31m %s \033[0m]\n\n" " * Configure jenkins server with SSL" "[" "Fail" && exit 1;;
+        esac
+    else
+        printf "\n${LINE}\n${RED}ERROR: Sorry, you must install nginx before configure SSL.${NC}\n\n"
+    fi
+fi
 printf "${LINE}\n\n${PURPLE}Packages Install Info:${NC}\n * Oracle Java Version:  \t[ ${GREEN}${JavaVer}${NC} ]\n"
 printf " * Jenkins Server Status:\t[ ${GREEN}Running${NC} ]\n"
 [ -z $initPasswd ] && printf " * Get Initial Admin Password:\t[ ${RED}Fail${NC} ]\n\n" && initPasswd=None || printf " * Get Initial Admin Password:\t[ ${GREEN}OK${NC} ]\n\n"
-printf "${LINE}\n\n${PURPLE}Jenkins Server Info:${NC}\n * Server site - ${GREEN}http://`GethostIPAddr`${NC}${RED}:8080${NC}\n * Init Password - ${RED}${initPasswd}${NC}\n\n"
-
+printf "${LINE}\n\n${PURPLE}Jenkins Server Info:${NC}\n * Server site - ${GREEN}${PROTOCOL}://`GethostIPAddr`${NC}${RED}:${PORT}${NC}\n * Init Password - ${RED}${initPasswd}${NC}\n\n"
