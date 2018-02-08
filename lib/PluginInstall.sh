ad885289f9c7e56774b0162e2101b510686a5528
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-08 08:59:59 +0800

    Update PluginInstall.sh

--- lib/PluginInstall.sh
+++ lib/PluginInstall.sh
@@ -1,13 +1,17 @@
 #!/bin/bash.
-. lib/GethostIPAddr.sh
+. GethostIPAddr.sh
+. declare_variables.sh
+
 PluginInstall()
-{ 
+{
+    [ ! -f suggested_plugin_list.json ] && printf "${RED}ERROR: file suggested_plugin_list.json is missing.${NC}\n" && exit 1
     case $1 in
         sug) local regx='\"suggested\": true';;
         all) local regx='\"name\": ';;
     esac
+    printf "\n${LINE}\n${PURPLE}Starting install Jenkins suggested plugins:${NC}\n${LINE}\n"
     for plugin in `grep -E $regx suggested_plugin_list.json | awk -F\" '{print $4}'`
     do  java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://`GethostIPAddr`:8080/ install-plugin $plugin
     done
-    return 0
+    printf "\n${LINE}\n\n" && return 0
 }
