00f27fb0dd19fb42b85d8c09fec8ac3166c6f52b
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-08 13:12:03 +0800

    Update PluginInstall.sh

--- lib/PluginInstall.sh
+++ lib/PluginInstall.sh
@@ -7,7 +7,7 @@ PluginInstall()
         full) local regx='\"name\": ';;
     esac
     printf "\n${LINE}\n${PURPLE}Starting install Jenkins suggested plugins:${NC}\n${LINE}\n"
-    for plugin in `grep -E $regx suggested_plugin_list.json | awk -F\" '{print $4}'`
+    for plugin in `grep -E "$regx" suggested_plugin_list.json | awk -F\" '{print $4}'`
     do  java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -s http://`GethostIPAddr`:8080/ install-plugin $plugin
     done
     printf "\n${LINE}\n\n" && return 0
