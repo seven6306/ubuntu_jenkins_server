ea3b1c8f7fff273d15cb00ddfffc7523ccebfe8a
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-09 21:28:19 +0800

    Create check_IP_format.py

---
+++ lib/check_IP_format.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+from sys import argv
+from os import system
+from socket import inet_aton
+
+def valid_ip(Addr):
+    try:
+        Addr2, tmp_Addr = '', []
+        inet_aton(Addr)
+        [tmp_Addr.append(int(i)) for i in Addr.split('.')]
+        Addr2 = ''.join('{0}{1}.'.format(Addr2, str(j)) for j in tmp_Addr)[:-1]
+        if system('ping -q {} -c 1 >> /dev/null 2>&1'.format(Addr2)) != 0:
+            raise Exception
+        return Addr2
+    except:
+        return False
+if __name__ == '__main__':
+    if len(argv) > 1:
+        print(valid_ip(argv[1]))
