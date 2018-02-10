58d7a610696e7f0c92e3c08047a9604e1388d6e8
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-10 15:48:07 +0800

    Update user_creator.py

--- lib/user_creator.py
+++ lib/user_creator.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 from sys import argv
-from os import remove
-from os.path import isfile
 from getpass import getpass
 
 if __name__ == '__main__':
@@ -23,8 +21,6 @@ if __name__ == '__main__':
         if len(argv) > 2:
             if argv[2] == '--showinfo':
                 print '{0};{1}'.format(username, password1)
-        if isfile('/tmp/account.cache'):
-            remove('/tmp/account.cache')
         with open('/tmp/account.cache', 'w') as cache:
             cache.write('username={0};password1={1}'.format(username, password1))
     else:
