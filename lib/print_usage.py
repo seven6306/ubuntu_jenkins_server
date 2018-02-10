9b2949e2ab1563330432082ea634cf42748dc874
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-10 16:34:05 +0800

    Update print_usage.py

--- lib/print_usage.py
+++ lib/print_usage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-with open('../README.md') as rf:
+with open('README.md') as rf:
     data = rf.read().split('```')
 for each_line in data[1].replace('javascript\n','').split('\n'):
     print each_line
