9e7260738a57b3c26c6f1dd156e2c52a73295a33
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-12 01:21:16 +0800

    Update NetworkConnTest.sh

--- lib/NetworkConnTest.sh
+++ lib/NetworkConnTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NetworkConnTest()
 {
-    python gethostIPaddr.py >> /dev/null 2>&1
+    python lib/gethostIPaddr.py >> /dev/null 2>&1
     [ $? -eq 255 ] && printf "%s\t%34s\033[0;31m%s\033[0m]\n" " * Network connection test       " "[" "Fail" && exit 1
     printf "%s\t%34s\033[0;32m %s \033[0m]\n" " * Network connection test         " "[" "OK"
     return 0
