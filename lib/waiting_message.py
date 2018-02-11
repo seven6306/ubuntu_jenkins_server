d52d2e83d1bcdad7d7e5d2f7d9c759d6e2caab41
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-11 21:25:39 +0800

    Create waiting_message.py

---
+++ lib/waiting_message.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from time import sleep
+from sys import argv, stdout
+
+def Waiting(message, delay):
+    for i in range(1,(int(delay) / 5) + 1):
+        for j in range(1,5):
+            if j == 1:
+                stdout.write(message + ' ')
+            stdout.write('.'+' '+"\b")
+            stdout.flush()
+            sleep(1)
+        stdout.write(' '*(len(message)+5)+'\r')
+    print ''
+
+if __name__ == '__main__':
+    try:
+        if len(argv) == 3:
+            message, delay = argv[1], argv[2]
+            if not delay.isdigit():
+                raise Exception
+            Waiting(message, delay)
+    except:
+        print 'Usage: python waiting_message.py [Message] [delay:seconds]'
