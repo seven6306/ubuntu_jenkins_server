2a5025335888a92f18f132b59a31113902e0f70e
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-12 00:32:29 +0800

    Update jenkins_server.sh

--- jenkins_server.sh
+++ jenkins_server.sh
@@ -91,8 +91,8 @@ if [ $NOASK -eq 0 ]; then
     fi
 fi
 [ -z "${username}" -o -z "${password1}" ] && python lib/user_creator.py "Create new admin user:" && . /tmp/account.cache && rm -f /tmp/account.cache
-printf "${PURPLE}Waiting for server apply admin user:${NC}\n"
-sleep 60 && echo "jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${username}\", \"${password1}\")" | sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -auth admin:${initPasswd} -s http://localhost:8080/ groovy =
+python lib/waiting_message.py "Waiting for server apply admin user" 60
+echo "jenkins.model.Jenkins.instance.securityRealm.createAccount(\"${username}\", \"${password1}\")" | sudo java -jar /var/cache/jenkins/war/WEB-INF/jenkins-cli.jar -auth admin:${initPasswd} -s http://localhost:8080/ groovy =
 [ $? -eq 0 ] && printf "%s\t%34s\033[0;32m %s \033[0m]\n\n" " * Apply new admin user to Jenkins  " "[" "OK" || printf "%s\t%34s\033[0;31m%s\033[0m]\n\n" " * Apply new admin user to Jenkins  " "[" "Fail"
 printf "${LINE}\n\n${PURPLE}Packages Install Info:${NC}\n * Oracle Java Version:  \t[ ${GREEN}${JavaVer}${NC} ]\n"
 printf " * Jenkins Server Status:\t[ ${GREEN}Running${NC} ]\n"
