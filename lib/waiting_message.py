843ea1cac8b5a7890adfafaa07f50d9d0eec0639
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-12 00:32:59 +0800

    Update waiting_message.py

--- lib/waiting_message.py
+++ lib/waiting_message.py
@@ -3,15 +3,16 @@ from time import sleep
 from sys import argv, stdout
 
 def Waiting(message, delay):
+    PURPLE, NC = '\033[1;35m', '\033[0m'
     for i in range(1,(int(delay) / 5) + 1):
         for j in range(1,5):
             if j == 1:
-                stdout.write(message + ' ')
-            stdout.write('.'+' '+"\b")
+                stdout.write(PURPLE + message + ' ' + NC)
+            stdout.write(PURPLE + '.'+' '+"\b" + NC)
             stdout.flush()
             sleep(1)
-        stdout.write(' '*(len(message)+5)+'\r')
-        stdout.write(' '*(len(message)+5)+'\r')
+        stdout.write(PURPLE + ' '*(len(message)+5)+'\r' + NC)
+        stdout.write(PURPLE + ' '*(len(message)+5)+'\r' + NC)
     print ''
 
 if __name__ == '__main__':
