1cb9df9ed1694945bb3a65f617ae40bd4a563a2f
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-12 17:07:44 +0800

    Create notification.py

---
+++ lib/notification.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+from sys import argv
+
+def notification(question, text):
+    c = 3
+    try:
+        while(c != 0):
+            c = c - 1
+            ans = raw_input(question)
+            if ans != '' and ans[0] in ['y', 'Y']:
+                print text
+                return True
+            elif ans != '' and ans[0] in ['n', 'N']:
+                return False
+            else:
+                print '\033[0;31mERROR: invalid answer, retry times remain: %d\033[0m' % c
+        return False
+    except KeyboardInterrupt:
+        return False
+if __name__ == '__main__':
+    if len(argv) == 3:
+        question, text = argv[1], argv[2]
+        if not notification(question, text):
+            raise SystemExit(5)
+    else:
+        print 'Usage: python notification.py [Question] [Text]'
