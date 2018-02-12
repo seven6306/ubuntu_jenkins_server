10d6f221c90f6cead467be8f38c865d483f68cb9
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-13 00:46:03 +0800

    Update update_server_IP.sh

--- update_server_IP.sh
+++ update_server_IP.sh
@@ -5,6 +5,7 @@
 [ "$1" = "-h" -o "$1" = "--help" ] && printf "Usage: sh update_server_IP.sh [OPTION]\n       -q,   --quiet       Force to update jenkins server IP address.\n" && exit 0
 python lib/checkPermission.py || exit 1
 python lib/checkInstall.py "jenkins_nginx" --remove "/etc/init.d/jenkins,/usr/sbin/nginx,/var/lib/jenkins,/usr/share/jenkins,/etc/nginx,/usr/share/nginx" || exit 1
+[ `grep -cE "proxy_redirect|proxy_read_timeout|https://" /etc/nginx/sites-enabled/default` -ne 2 ] && printf "${RED}ERROR: Jenkins server is not configure with SSL.${NC}\n" && exit 1
 case $1 in
     -q|--quiet) server_name=`python lib/gethostIPaddr.py`;;
     *) printf "To update jenkins server name (Default: localhost),\n" && read -p "please input IP or domain name: " server_name;;
