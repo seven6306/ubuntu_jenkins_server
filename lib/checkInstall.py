0d1a31fcc2d780301033e3acac892f82d4d96387
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-12 10:29:57 +0800

    Create checkInstall.py

---
+++ lib/checkInstall.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+from sys import argv, exit
+from os.path import isfile, isdir
+
+def checkInstall(pkgName, action, files):
+    x, f_list = 0, files.split(',')
+    for i in f_list:
+        if isfile(i) or isdir(i):
+            x = x + 1
+    if '--install' in action and x != 0:
+        print "\033[0;31mERROR: {} is already installed.\033[0m".format(pkgName)
+        return False
+    elif '--remove' in action and x != len(f_list):
+        print "\033[0;31mERROR: {} is not installed.\033[0m\n".format(pkgName)
+        return False
+    print "%s\t%34s\033[0;32m %s \033[0m]" % (" * Check if {} is installed      ".format(pkgName), "[", "OK")
+
+if __name__ == '__main__':
+    try:
+        if len(argv) == 4:
+            pkgName, action, files = argv[1], argv[2], argv[3]
+            if action not in ['--install', '--remove']:
+                raise SyntaxError
+            if not checkInstall(pkgName, action, files):
+                raise SystemExit
+        else:
+            raise SyntaxError
+    except SystemExit:
+        exit(2)
+    except SyntaxError:
+        print 'Usage: python checkInstall.py [Package] [Action] [Files]\n       e.g., python checkInstall.py Jenkins --remove "/etc/init.d/jenkins,/var/lib/jenkins,/usr/share/jenkins"' 
+    except Exception as err:
+        print str(err)
