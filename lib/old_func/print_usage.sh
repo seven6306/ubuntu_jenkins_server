7aafa9f8b2a2e1694fdf59b1f2dd76b4ec06e8de
seven6306 <seven6306@icloud.com>
2018-02-11 23:20:59 -0800

    manage library

---
+++ lib/old_func/print_usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+print_usage()
+{
+    [ ! -f README.md ] && printf "\033[0;31mERROR: file README.md is not found.\033[0m\n" && return 0
+    line_on=$(($(grep -n "\`\`\`javascript" README.md | cut -d \: -f1)+1))
+    line_down=$(($(grep -nE "^\`\`\`$" README.md | cut -d \: -f1)-1))
+    for i in `seq $line_on $line_down`
+    do  cat README.md | sed -n ${i}p
+    done
+    printf "\n"
+}
