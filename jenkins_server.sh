83d43583d63fb25dbed1decce642195b23387f63
seven6306 <31332405+seven6306@users.noreply.github.com>
2018-02-23 14:26:07 +0800

    Update jenkins_server.sh

--- jenkins_server.sh
+++ jenkins_server.sh
@@ -9,12 +9,12 @@ PORT=8080
 PROTOCOL=http
 
 [ "$1" = '?' -o "$1" = "-h" -o "$1" = "--help" ] && python lib/print_usage.py README.md && exit 0
-python lib/checkPermission.py || exit 1
 if [ "$1" = "-y" -o "$1" = "--yes" ]; then
     [ "${2}" = "admin" ] && printf "${RED}ERROR: can not create super user.${NC}\n" && exit 1
     [ ! -z "${2}" -a ! -z "${3}" ] && [ `echo $2 | grep -c "username="` -ne 0 -a `echo $3 | grep -c "password="` -ne 0 ] && username=`echo $2 | cut -d \= -f2` && password1=`echo $3 | cut -d \= -f2`
     NOASK=1
 fi
+python lib/checkPermission.py || exit 1
 if [ $# -ne 0 -a $NOASK -ne 1 ]; then
     python lib/checkInstall.py jenkins --remove "/etc/init.d/jenkins,/var/lib/jenkins,/usr/share/jenkins" || exit 1
     case $1 in
@@ -33,7 +33,7 @@ if [ $# -ne 0 -a $NOASK -ne 1 ]; then
                  sh update_server_IP.sh -q;;
     -s|--sslconf) [ "$2" = "--quiet" ] && NOASK=1
                   SSLconfigure;;
-    * ) python lib/print_usage.py README.md;;
+    * ) printf "${RED}ERROR: try '-h or --help' for more information.${NC}\n";;
     esac
     exit 0
 fi
